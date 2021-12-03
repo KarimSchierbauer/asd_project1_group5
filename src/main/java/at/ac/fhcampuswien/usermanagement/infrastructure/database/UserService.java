5c758214bb4ef0c9d7e7678342f1325e8181253f
Fumeaux <onger.max@gmail.com>
2021-12-03 20:20:52 +0100

    fix login check

--- src/main/java/at/ac/fhcampuswien/usermanagement/infrastructure/database/UserService.java
+++ src/main/java/at/ac/fhcampuswien/usermanagement/infrastructure/database/UserService.java
@@ -2,7 +2,6 @@ package at.ac.fhcampuswien.usermanagement.infrastructure.database;
 
 import at.ac.fhcampuswien.usermanagement.models.NewUserDTO;
 import at.ac.fhcampuswien.usermanagement.util.utility;
-import org.mindrot.jbcrypt.BCrypt;
 
 import java.sql.*;
 
@@ -23,40 +22,39 @@ public class UserService {
 
     public boolean insertUser(NewUserDTO newUserDTO){
         Connection connection = getConnection();
-        boolean usernameTaken= compareUsername(newUserDTO.getUsername());
-        if(!usernameTaken){
-            String rawCommand = "INSERT INTO userSchema.userEntity (firstname,lastname,username,password) VALUES ('%s', '%s', '%s', '%s');";
-            String hashpw = utility.hashPW(newUserDTO.getPassword());
 
-            //if (BCrypt.checkpw(newUserDTO.getPassword(), hashpw)) {
-            //    System.out.println("Password is fine");
-            //}
-
-            try {
-                Statement stmt = connection.createStatement();
-                String sql = String.format(rawCommand, newUserDTO.getFirstname(), newUserDTO.getLastname(), newUserDTO.getUsername(), hashpw);
-                System.out.println(sql);
-                stmt.executeUpdate(sql);
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }else{
+        boolean usernameTaken = usernameAlreadyTaken(newUserDTO.getUsername());
+        if(usernameTaken){
             return false;
         }
 
-        return true;
+        String rawCommand = "INSERT INTO userSchema.userEntity (firstname,lastname,username,password) VALUES ('%s', '%s', '%s', '%s');";
+        String hashpw = utility.hashPW(newUserDTO.getPassword());
+
+        try {
+            Statement stmt = connection.createStatement();
+            String sql = String.format(rawCommand, newUserDTO.getFirstname(), newUserDTO.getLastname(), newUserDTO.getUsername(), hashpw);
+            System.out.println(sql);
+            stmt.executeUpdate(sql);
+            connection.close();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 
-    public boolean compareUsername(String username){
+    public boolean usernameAlreadyTaken(String username){
         Connection connection = getConnection();
 
         try {
-            PreparedStatement rawCommand = connection.prepareStatement("SELECT username FROM userSchema.userEntity WHERE username=?");
+            PreparedStatement rawCommand = connection.prepareStatement("SELECT username FROM userSchema.userEntity WHERE username = ?");
             rawCommand.setString(1, username);
             ResultSet rs = rawCommand.executeQuery();
             while (rs.next()) {
-                if (rs.getString("username") == null || rs.getString("username").isEmpty()) {
+                String usernameFromDb = rs.getString("username");
+                if (usernameFromDb == null || usernameFromDb.isEmpty()) {
                     return false;
                 } else {
                     return true;
@@ -78,10 +76,13 @@ public class UserService {
             rawCommand.setString(1, newUserDTO.getUsername());
             ResultSet rs = rawCommand.executeQuery();
             while (rs.next()) {
-                if (rs.getString("username") == newUserDTO.getUsername() && utility.checkPW(newUserDTO.getPassword(), rs.getString("password"))) {
+                String usernameFromDb = rs.getString("username");
+                if(!usernameFromDb.equals(newUserDTO.getUsername())){
+                    continue;
+                }
+                String passwordFromDb = rs.getString("password");
+                if (utility.checkPW(newUserDTO.getPassword(), passwordFromDb)) {
                     return true;
-                } else {
-                    return false;
                 }
             }
             rawCommand.close();
