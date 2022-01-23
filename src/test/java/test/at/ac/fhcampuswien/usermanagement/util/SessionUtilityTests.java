87cfe5b0ebeedad24e191a2c250e00132859c099
Fumeaux <onger.max@gmail.com>
2022-01-23 22:46:03 +0100

    add test for dont find inactive user

--- src/test/java/test/at/ac/fhcampuswien/usermanagement/util/SessionUtilityTests.java
+++ src/test/java/test/at/ac/fhcampuswien/usermanagement/util/SessionUtilityTests.java
@@ -1,9 +1,14 @@
 package test.at.ac.fhcampuswien.usermanagement.util;
 
 import at.ac.fhcampuswien.usermanagement.models.NewUserDTO;
+import at.ac.fhcampuswien.usermanagement.util.Session;
 import at.ac.fhcampuswien.usermanagement.util.SessionUtility;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -43,6 +48,29 @@ public class SessionUtilityTests {
         NewUserDTO foundUser = SessionUtility.getUser(sessionID);
 
         assertEquals(newUser, foundUser);
+    }
+
+    @Test
+    public void getUser_dontFindInactiveUser(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MINUTE, -1);
+        Date sessionValidDate = calendar.getTime();
+
+        Date currentDate = Calendar.getInstance().getTime();
+
+        try (MockedStatic<SessionUtility> sessionUtilityMock = Mockito.mockStatic(SessionUtility.class, Mockito.CALLS_REAL_METHODS)){
+            sessionUtilityMock.when(() -> SessionUtility.sessionValidUntil())
+                    .thenReturn(sessionValidDate);
+            sessionUtilityMock.when(() -> SessionUtility.currentDate())
+                    .thenReturn(currentDate);
+
+            NewUserDTO newUser = new NewUserDTO();
+
+            UUID sessionID = SessionUtility.createNewSessionForUser(newUser);
+            NewUserDTO foundUser = SessionUtility.getUser(sessionID);
+
+            assertEquals(null, foundUser);
+        }
     }
 
     @Test
